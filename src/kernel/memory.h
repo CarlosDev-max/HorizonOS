7b2426fd26a38538a9f406c54a58f80a727aa47f
CarlosDev-max <carlitosmanzi16@gmail.com>
2026-09-12 23:04:52 -0300

    feat: memory manager header

---
+++ src/kernel/memory.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdint.h>
+#include <stddef.h>
+
+void  mem_init(void);
+void* mem_alloc(size_t size);
+void  mem_free(void* ptr);
+void  mem_stats(uint32_t* used, uint32_t* free);
+
