cfb65ad3341f5f7d02a62b074ac0562148a7cb27
CarlosDev-max <carlitosmanzi16@gmail.com>
2026-09-14 19:13:55 -0300

    feat: home screen header

--- src/apps/home.h
+++ src/apps/home.h
@@ -1,6 +1,7 @@
 #pragma once
+#include <stdint.h>
 
 void app_home_launch(void);
 void app_home_draw(void);
 void app_home_touch(int16_t x, int16_t y);
-
+void app_home_tick(void);   // call each frame for animation
