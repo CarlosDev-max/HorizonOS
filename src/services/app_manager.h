22db525d5d257348758ff7958f2c8ad9c4cbacf2
CarlosDev-max <carlitosmanzi16@gmail.com>
2026-09-19 13:25:32 -0300

    fix: add stdint.h for uint8_t

--- src/services/app_manager.h
+++ src/services/app_manager.h
@@ -1,12 +1,12 @@
 #pragma once
+#include <stdint.h>
 
 typedef void (*app_launch_fn_t)(void);
 
 typedef struct {
-    const char*    name;
+    const char*     name;
     app_launch_fn_t launch;
 } AppDef;
 
 void app_manager_register(const char* name, app_launch_fn_t launch);
 void app_manager_launch(const char* name);
-
